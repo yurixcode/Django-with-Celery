765535938df0e30af1e851a2500b4310b28c8618
Ernesto Escribá <eescriba@movisat.com>
2019-10-22 13:31:52 +0200

    Script: launch multiple tasks with api

--- celeryapp/celery.py
+++ celeryapp/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from kombu import Exchange, Queue
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryapp.settings')
@@ -12,6 +13,13 @@ app = Celery('celeryapp')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.task_queues = (
+    Queue('default', Exchange('default'), routing_key='default'),
+)
+app.conf.task_default_queue = 'default'
+app.conf.task_default_exchange_type = 'direct'
+app.conf.task_default_routing_key = 'default'
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
