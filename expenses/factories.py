7a89bbf6eed7d05c7bff7a855bc2410d2cc6770c
Yuri <yferreira@movisat.com>
2020-03-08 21:48:26 +0100

    Factorias, 1º parte

--- expenses/factories.py
+++ expenses/factories.py
@@ -1,3 +1,6 @@
+# Django
+from django.contrib.auth import get_user_model
+
 # Factory
 import factory
 from factory import Faker
@@ -6,6 +9,21 @@ from factory.django import DjangoModelFactory
 # Models
 from expenses.models import Expense
 
+User = get_user_model()
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+        
+    first_name = factory.Faker('name')
+    username = factory.lazy_attribute(lambda o: '%s' % o.first_name)
+    email = factory.LazyAttribute(lambda o: '%s@example.com' % o.first_name)
+
 
 class ExpenseFactory(DjangoModelFactory):
-    pass
+    class Meta:
+        model = Expense
+
+    user = User.objects.last()
+    name = factory.Sequence(lambda n: 'Compra impulsiva #%03s' % n)
+    cost = 250.00
--- expenses/models.py
+++ expenses/models.py
@@ -6,11 +6,11 @@ from django.db import models
 class Expense(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    name = models.CharField(max_length=20)
-    cost = models.DecimalField(max_digits=25, decimal_places=5)
+    name = models.CharField(max_length=50)
+    cost = models.DecimalField(max_digits=25, decimal_places=3)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.name} {self.cost}'
+        return f'{self.name} - {self.cost}'
